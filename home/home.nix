72af81156414feba20a870f61e3138b9a4c75acc
Makoto Ito <m4k070@pm.me>
2025-12-02 21:17:34 +0900

    fix

--- home/home.nix
+++ home/home.nix
@@ -21,12 +21,30 @@
      };
 
      programs.home-manager.enable = true;
-     programs.zsh.enable = true;
-     programs.git.enable = true;
+     programs.zsh = {
+       enable = true;
+       autocd = true;
+       enableCompletion = true;
+       # enableAutosuggestions = true;
+       syntaxHighlighting.enable = true;
+       shellAliases = {
+         cat = "bat";
+       };
+     };
+     programs.git = {
+       enable = true;
+     };
+     programs.gh = {
+       enable = true;
+       extensions = with pkgs; [gh-markdown-preview];
+     };
+     programs.starship = {
+       enable = true;
+     };
 
-# dotfiles を管理するならここで設定追加
-# home.file.".zshrc".text = ''
-#   export PATH=$HOME/.local/bin:$PATH
-# '';
-      news.display = "silent";
+     # dotfiles を管理するならここで設定追加
+     # home.file.".zshrc".text = ''
+     #   export PATH=$HOME/.local/bin:$PATH
+     # '';
+     news.display = "silent";
   }
