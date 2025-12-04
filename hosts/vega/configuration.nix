18e8d321989edba47a13a6b534ccd83e5c25813c
Makoto Ito <m4k070@pm.me>
2025-12-04 22:53:00 +0900

    fix

--- hosts/vega/configuration.nix
+++ hosts/vega/configuration.nix
@@ -14,10 +14,15 @@
 
   networking.hostName = "vega"; # Define your hostname.
   
+  environment.systemPackages = [
+    #pkgs.niri
+  ];
+  
   services.xserver.videoDrivers = ["nvidia"];
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
+    powerManagement.finegrained = false;
     open = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
--- nixos/base.nix
+++ nixos/base.nix
@@ -53,6 +53,7 @@
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-mozc
+      fcitx5-gtk
       kdePackages.fcitx5-qt
       qt6Packages.fcitx5-configtool
     ];
