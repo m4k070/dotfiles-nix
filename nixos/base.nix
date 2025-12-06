6df82c17cca551afb535a004671665090317b90c
Makoto Ito <m4k070@pm.me>
2025-12-06 21:04:21 +0900

    fix

--- nixos/base.nix
+++ nixos/base.nix
@@ -137,6 +137,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     alacritty
+    btop
     bitwarden-desktop
     niri
     starship
