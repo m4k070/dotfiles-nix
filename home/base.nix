9c9e2d43885c921833839371caf5e191c0798f91
Makoto Ito <m4k070@pm.me>
2025-12-25 07:42:28 +0900

    add tmux config

--- home/base.nix
+++ home/base.nix
@@ -157,6 +157,41 @@ in {
     enable = true;
   };
 
+  programs.tmux = {
+    enable = true;
+    mouse = true;
+    keyMode = "vi";
+    escapeTime = 0;
+    clock24 = true;
+    terminal = "screen-256color";
+    extraConfig = ''
+      # window移動
+      bind -n M-l next-window
+      bind -n M-h previous-window
+      
+      bind -n M-j switch-client -n
+      bind -n M-k switch-client -p
+      
+      # pane移動
+      bind -n C-h select-pane -L
+      bind -n C-j select-pane -D
+      bind -n C-k select-pane -U
+      bind -n C-l select-pane -R
+      
+      # TERMを設定
+      set -g terminal-overrides 'xterm:colors=256'
+      
+      # スクロールアップするとコピーモードに入る
+      bind-key -n WheelUpPane if-shell -F -t = "#{mouse_any_flag}" "send-keys -M" "if -Ft= '#{pane_in_mode}' 'send-keys -M' 'select-pane -t=; copy-mode -e; send-keys -M'"
+      
+      # 最後までスクロールダウンするとコピーモードを抜ける
+      bind-key -n WheelDownPane select-pane -t= \; send-keys -M
+      
+      # リロード
+      bind r source-file ~/.tmux.conf
+    '';
+  };
+
   xdg.configFile."starship.toml".source = ../configs/starship/starship.toml;
 
   xdg.configFile."nvim" = {
