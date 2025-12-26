dfda14f6ce7e96ebff927214b58282d26a1fc230
Makoto Ito <m4k070@pm.me>
2025-12-27 01:42:00 +0900

    update

--- configs/neovim/lua/plugins/core.lua
+++ configs/neovim/lua/plugins/core.lua
@@ -196,6 +196,16 @@ return {
     end,
   },
   "ionide/ionide-vim",
+  {
+    "nvimdev/lspsaga.nvim",
+    config = function()
+      require("lspsaga").setup({})
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    }
+  },
 
   {
     -- フォーマッタ
