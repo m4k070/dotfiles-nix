49870422fc6d8888ec2a532a494c444dfd89b86b
Makoto Ito <makoto.ito@tsukasa-ind.co.jp>
2026-01-04 16:13:35 +0900

    update

--- home/work.nix
+++ home/work.nix
@@ -53,4 +53,13 @@
         ];
       };
     };
+
+    programs.vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        dracula-theme.theme-dracula
+        vscodevim.vim
+        yzhang.markdown-all-in-one
+      ];
+    };
   }
