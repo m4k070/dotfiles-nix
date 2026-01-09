f2c69c067d2fb114888064d47f24c4fff40981c0
Makoto Ito <makoto.ito@tsukasa-ind.co.jp>
2026-01-09 14:34:53 +0900

    update

--- nixos/base.nix
+++ nixos/base.nix
@@ -87,11 +87,9 @@
   };
   services.udev.extraRules = ''
       ACTION=="remove",\
-       ENV{ID_BUS}=="usb",\
-       ENV{ID_MODEL_ID}=="0407",\
-       ENV{ID_VENDOR_ID}=="1050",\
-       ENV{ID_VENDOR}=="Yubico",\
-       RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+      ENV{ID_MODEL_ID}=="0407",\
+      ENV{ID_VENDOR_ID}=="1050",\
+      RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
   '';
 
   # NTP
