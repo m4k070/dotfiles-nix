ec76956ff1fa317ac81b3065102fe9c284ebe4fb
Makoto Ito <m4k070@pm.me>
2026-02-01 22:53:15 +0900

    update

--- hosts/vega/configuration.nix
+++ hosts/vega/configuration.nix
@@ -18,6 +18,15 @@
     protonup-qt
     sunshine
   ];
+
+  services.pipewire.extraConfig.pipewire."92-low-latency" = {
+    "context.properties" = {
+      "default.clock.rate" = 48000;
+      "default.clock.quantum" = 32;
+      "default.clock.min-quantum" = 32;
+      "default.clock.max-quantum" = 32;
+    };
+  };
   
   services.xserver.videoDrivers = ["nvidia"];
   services.sunshine = {
