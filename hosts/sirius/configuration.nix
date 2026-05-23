55e1178e234187f9ea6c8c41ac3ae2e36fd25501
Makoto Ito <m4k070@pm.me>
2026-05-23 21:34:57 +0900

    update

--- hosts/sirius/configuration.nix
+++ hosts/sirius/configuration.nix
@@ -33,5 +33,27 @@
     enable = true;
   };
 
+  # ネットワークプリンター自動検出（mDNS）
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  # RICOH IM C3500F（IPP Everywhere対応のためドライバー不要）
+  hardware.printers = {
+    ensurePrinters = [{
+      name = "RICOH_IM_C3510F";
+      location = "Office";
+      deviceUri = "ipp://192.168.1.207/ipp/print";
+      # "everywhere"はlpadminのIPP属性バリデーションが日本語テキストを拒否するため静的PPDを使用
+      # pwgrast(Generic IPP Everywhere)は送信データエラーになるためPostScriptを使用
+      model = "drv:///sample.drv/generic.ppd";
+      ppdOptions = {
+        PageSize = "A4";
+      };
+    }];
+    ensureDefaultPrinter = "RICOH_IM_C3500F";
+  };
 
 }
